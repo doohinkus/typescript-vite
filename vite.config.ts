af8f8ff3969ad9d9b0ba0387a04c054768b8e2a6
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2025-05-30 21:43:25 -0700

    add domain

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/vanilla-math/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+}) 
